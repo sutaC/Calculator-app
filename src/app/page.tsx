ab4113fd116b114e402d69c1cf606e3d9fa2fa62
Antoni Siekański <catus.pl@gmail.com>
2023-11-07 17:01:47 +0100

    Reading board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,59 @@ import Header from "@/components/header";
 import Display from "@/components/display";
 import Board from "@/components/board";
 import styles from "./page.module.css";
-import { useEffect } from "react";
-
-function handleThemeChange(selectedOption: number) {
-	document.body.className = "";
-	const theme = `color-scheme-${selectedOption}`;
-	document.body.classList.add(theme);
-	localStorage.setItem("theme", String(selectedOption));
-}
-
-function handleBoardClick(event: any) {
-	const { btn } = event.detail;
-	if (!btn) {
-		console.error("Could not read board click");
-	}
-
-	console.log(btn);
-}
+import { useEffect, useState } from "react";
 
 export default function Home() {
+	let bd = "";
+	const [board, setBoard] = useState(bd);
+
+	function handleBoardClick(event: any) {
+		const { btn } = event.detail;
+		if (!btn) {
+			console.error("Could not read board click");
+		}
+
+		const sides = bd.split(/[x\/+-]/);
+		const last = sides[sides.length - 1];
+
+		switch (btn) {
+			case "RESET":
+				bd = "";
+				break;
+			case "DEL":
+				if (bd.length > 0) {
+					bd = bd.substring(0, bd.length - 1);
+				}
+				break;
+			case "+" || "x" || "/" || "-":
+				// TODO
+				if (
+					bd.includes("+") ||
+					bd.includes("-") ||
+					bd.includes("x") ||
+					bd.includes("/")
+				) {
+					break;
+				}
+				bd += btn === "x" ? "*" : btn;
+				break;
+			default:
+				if (last[last.length - 1] === "0" && last.length <= 1) {
+					bd = bd.substring(0, bd.length - 1);
+				}
+				bd += btn;
+				break;
+		}
+		setBoard(bd);
+	}
+
+	function handleThemeChange(selectedOption: number) {
+		document.body.className = "";
+		const theme = `color-scheme-${selectedOption}`;
+		document.body.classList.add(theme);
+		localStorage.setItem("theme", String(selectedOption));
+	}
+
 	useEffect(() => {
 		const themeNumber = Number(localStorage.getItem("theme") ?? 1);
 		document.body.className = "";
@@ -39,7 +73,7 @@ export default function Home() {
 			<main className={styles.main}>
 				<Header></Header>
 
-				<Display text='2137'></Display>
+				<Display text={board}></Display>
 
 				<Board></Board>
 			</main>
--- src/components/display.tsx
+++ src/components/display.tsx
@@ -1,5 +1,9 @@
 import styles from "./display.module.css";
 
 export default function Display(params: { text: string }) {
-	return <div className={styles.display}>{params.text}</div>;
+	return (
+		<div className={styles.display}>
+			<span>{params.text}</span>
+		</div>
+	);
 }
